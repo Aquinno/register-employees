68c1209ec7c5dbe50f957a0f64176cd69237d20e
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-22 22:29:37 -0300

    Add files via upload

---
+++ RegisterEmployees/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterEmployees {
+    internal class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Employee(string name, int id, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public void Increase(double percentage) {
+            Salary += Salary * percentage / 100;
+        }
+    }
+}
+
+
---
+++ RegisterEmployees/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace RegisterEmployees {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("How many employees: ");
+            int count = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < count; i++) {
+                Console.WriteLine($"Employee #{i+1}: ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+                employees.Add(new Employee(name, id, salary));
+            }
+            Console.Write("Enter the employee ID to increase salary: ");
+            int idIncrease = int.Parse(Console.ReadLine());
+            Console.Write("Enter the percentage: ");
+            double percentage = double.Parse(Console.ReadLine());
+            Employee employee = employees.Find(x => x.Id == idIncrease);
+            if (employee != null) {
+                employee.Increase(percentage);
+            }
+            else {
+                Console.WriteLine("Employee not found");
+            }
+            Console.WriteLine("Update list of employees: ");
+            foreach (Employee obj in employees) {
+                Console.WriteLine(obj.Id + ", "+ obj.Name + ", " + obj.Salary);
+            }
+        }
+    }
+}
